e50266a4123ec139cb99580054a75d165e0f0f6d
AgustinNunez1 <agustinpc9@gmail.com>
2026-01-19 17:59:04 -0800

    Add files via upload

---
+++ sumTwoInts.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ostream>
+#include <cmath>
+using namespace std;
+
+
+int main () {
+
+    int number1;
+    int number2;
+
+    cout << "Hi, I will be adding two of your numbers today. Please give me the first number: ";
+    cin >> number1;
+    cout << "Thank You, Now the second number:";
+    cin >> number2;
+
+    cout << "Awsome, your two numbers added together are now : " << number1 + number2 << endl;
+
+    
+    return 0;
+}
