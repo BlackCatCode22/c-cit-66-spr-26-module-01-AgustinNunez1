651c65d8d94469f4dc485d50bc74f74fb064358a
AgustinNunez1 <agustinpc9@gmail.com>
2026-01-19 17:39:50 -0800

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ostream>
+#include <cmath>
+using namespace std;
+
+
+int main () {
+
+    int number;
+
+    cout << "Hello there, give me a number and I will double it: ";
+    cin >> number;
+
+    cout << "your number is now " << number * 2 << endl;
+    return 0;
+}
