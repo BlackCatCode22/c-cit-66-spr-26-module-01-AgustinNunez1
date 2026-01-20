ae47176286147d37b7c89ff3c7f36a7c4bce7908
AgustinNunez1 <agustinpc9@gmail.com>
2026-01-19 17:50:08 -0800

    Add files via upload

---
+++ tripleInt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+
+    int triplednumber;
+
+    cout << "Enter a number and I will triple it: ";
+    cin >> triplednumber;
+
+    cout << "Your new tripled number is now : " << triplednumber * 3 << endl;
+
+    return 0;
+
+}
